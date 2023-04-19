c1679f72b97ef1a2e892f7018d318513765d8766
Sergiu Mironescu Iancu <sergiumironescuiancu1@MacBook-Pro-6.local>
2023-04-19 16:54:28 +0200

    roadmap comments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@
 import React from 'react';
 import Input from './Inputs.js';
 
+// CRIS
+// Card Component - Cris
+// UserTitle Component - Cris
+// UserSubTitle Component - Cris
+// UserDescription Component - Cris
+// UserLocation Component - Cris
+
+// SERGIU
+// Search Bar - Sergiu
+// Avatar Component inside Card Component - Sergiu
+
 function App() {
   return (
     <div className="App">
